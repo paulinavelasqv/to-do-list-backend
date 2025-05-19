615122fea975fb6770f4048cb6d9330f89db4e54
Paulina <paulinavelasqv@gmail.com>
2025-05-19 18:27:07 -0400

    Se parametriza URL de DB

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
+
+const dbUrl = process.env.DB_URL;
 
 const connection = async (req, res) => {
 
     try {
-        await mongoose.connect("mongodb://localhost:27017/to_do_list");
+        await mongoose.connect(dbUrl);
         console.log("Conectado correctamente a la base de datos to_do_list");
     } catch (error) {
         console.log(error);
